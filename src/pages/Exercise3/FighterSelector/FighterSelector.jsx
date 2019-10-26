cf9034855c1fcac2baa489c2ba776d642d9099ce
andrecoghi@gmail.com <andrecoghi@gmail.com>
2019-10-26 16:32:24 -0300

    fix setState

--- src/pages/Exercise3/FighterSelector/FighterSelector.jsx
+++ src/pages/Exercise3/FighterSelector/FighterSelector.jsx
@@ -20,7 +20,7 @@ function FighterSelector() {
                     time: (new Date()).toLocaleTimeString()
                 }
             ]);
-        }, 1);
+        }, 1); 
     }, [fighter])
 
     return (
