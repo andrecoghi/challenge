841982b61d069e41e7f35f610a90b452d954a1fb
andrecoghi@gmail.com <andrecoghi@gmail.com>
2019-10-26 16:18:54 -0300

    decrease timeout to show messages

--- src/pages/Exercise3/FighterSelector/FighterSelector.jsx
+++ src/pages/Exercise3/FighterSelector/FighterSelector.jsx
@@ -20,7 +20,7 @@ function FighterSelector() {
                     time: (new Date()).toLocaleTimeString()
                 }
             ]);
-        }, 1000);
+        }, 1);
     }, [fighter])
 
     return (
